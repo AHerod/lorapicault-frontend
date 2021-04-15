9e0e6e14042166ea033ac5f8eded4f2b3c9d6e5e
AHerod <agn.j.herod@gmail.com>
2021-04-16 00:21:12 +0200

    setup tailwind, add prettier, remove unneeded readme files

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  jit: true,
+  purge: [],
+  darkMode: false,
+  theme: {
+    colors: {
+      primary: "#2c2c2c",
+      secondary: "#fbbaa1",
+      light: "#c5c5c5",
+      dark: "#6a6a6a",
+      info: "",
+      success: "",
+      warning: "",
+      danger: "",
+    },
+    fontFamily: {
+      primary: ["Lato", "sans-serif"],
+      secondary: ["Alex Brush", "cursive"],
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
