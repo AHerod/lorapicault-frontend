728c45590a94307c1bfaf3ff82dbe2c918ac3207
AHerod <agn.j.herod@gmail.com>
2021-04-18 19:25:28 +0200

    add ERROR page

---
+++ layouts/error.vue
@@ -0,0 +1,6 @@
+<template lang="pug">
+  div(class="min-h-screen flex justify-center items-center flex-col")
+    h1(class="font-primary text-9xl mb-8")
+      | Page not found
+    nuxt-link(to="/") Go back to homepage
+</template>
