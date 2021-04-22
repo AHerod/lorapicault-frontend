07c142675214efb3455cb3d2f82b28e59e6b4783
AHerod <agn.j.herod@gmail.com>
2021-04-22 23:52:08 +0200

    add image gallery grid

--- components/sections/Gallery.vue
+++ components/sections/Gallery.vue
@@ -2,9 +2,9 @@
   section(class="md:px-0").bg-primary.text-white.py-28
     div(class="flex justify-center align-center flex-wrap")
       span(v-for="filter in filters" class="text-sm sm:text-lg p-2 md:px-8 uppercase") {{filter}}
-    div(class="grid grid-cols-4 gap-6 md:gap-8 px-4 md:px-8  pt-6 md:px-24")
-      div(class="bg-primary" v-for="image in images" :class=`['col-span-'+image.size * 2, 'md:col-span-'+image.size]`)
-        img(:src="image.url" class="w-full h-auto block")
+    div(class="grid grid-cols-4 gap-4 md:gap-8 px-4 md:px-8  pt-6 md:px-24")
+      div(:style="`height: ${height}px`" class="relative w-full" v-for="image in images" :class=`['col-span-'+image.size * 2, 'md:col-span-'+image.size, image.size === 1 ? 'square' : '']`)
+        img(ref="square" :style="`height: ${height}px`" :src="image.url" class="w-full block absolute transform object-cover -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2")
 
 </template>
 
@@ -12,6 +12,7 @@
 export default {
   data() {
     return {
+      height: 50,
       filters: [
         'All', 'Extension', 'Lamination', 'Nature', 'Tinting'
       ],
@@ -53,5 +54,26 @@ export default {
         }]
     };
   },
+  created() {
+    if (process.browser) {
+      window.addEventListener("resize", this.updateElementHeight);
+    }
+  },
+  destroyed() {
+    if (process.browser) {
+      window.removeEventListener("resize", this.updateElementHeight);
+    }
+  },
+  mounted() {
+    this.height = this.getElementHeight(this.$refs.square.[1].clientWidth)
+  },
+  methods: {
+    getElementHeight: function (width) {
+      return width;
+    },
+    updateElementHeight: function () {
+      this.height = this.getElementHeight(this.$refs.square.[1].clientWidth)
+    }
+  }
 };
 </script>
