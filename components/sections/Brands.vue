bbacd3e7d3de0a008a333a1397e540530fbb7e75
aherod <agn.j.herod@gmail.com>
2021-04-26 15:29:09 +0200

    add 'Services' page

--- components/sections/Brands.vue
+++ components/sections/Brands.vue
@@ -1,8 +1,7 @@
 <template lang="pug">
-  section(class="md:px-0 py-14 md:py-28").px-4.grid.grid-cols-2.gap-x-8.bg-light
-    .grid(class="col-span-2 md:col-span-2 grid-cols-1 md:grid-cols-2 lg:grid-cols-4")
-      div(v-for="(item,index) in items" class="flex flex-col items-center justify-center text-center mb-10")
-        img(:src="require(`@/assets/images/icons/makeup_${item.icon}.png`)" class="w-14 h-14 lg:w-16 lg:h-16")
+  section(class="md:px-0 py-14 md:py-18").px-4.grid.grid-cols-2.gap-x-8.bg-light
+    .grid(class="col-span-2 md:col-span-2 grid-rows-3 md:grid-rows-1 grid-cols-1 md:grid-cols-3 flex justify-item center items-center")
+      img(v-for="(image, index) in items" :key="index" :src="require(`@/assets/images/brands/${image.path}.png`)" class="px-10")
 </template>
 
 <script>
@@ -10,10 +9,9 @@ export default {
   data() {
     return {
       items: [
-        {title: 'ALWAYS BEAUTIFUL EYES', info: 'Experienced craftsmen, modern technologies, consultation.', icon: 1},
-        {title: 'NO MORE MAKEUP', info: 'With extended eyelashes you can walk up to 4 weeks.', icon: 2},
-        {title: 'HYPOALLERGENIC', info: 'Work without gluing, warranty, premium materials.', icon: 3},
-        {title: 'YOU CAN SWIM IN THE SEA', info: 'We are distinguished by an individual approach to each client', icon: 4}
+        {path: 'bb', title: ''},
+        {path: 'lm', title: ''},
+        {path: 'ga', title: ''},
       ]
     }
   }
