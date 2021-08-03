c8f2088f7b49bbcddc789b9e75e2fa20ac69ef08
AHerod <agn.j.herod@gmail.com>
2021-08-03 23:01:24 +0200

    add Home to the mobile menu

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   nav(class="uppercase w-full grid grid-cols-12 absolute z-20").text-white
-    div(class="col-span-4")
+    div(class="col-span-4 z-50")
       NuxtLink(to="/")
         img(:src="require(`@/assets/images/logo_placeholder.png`)" class="md:w-4/5 mt-2")
     div(:class="open ? 'bg-primary' : ''" class="transition-all duration-500 ease-in-out fixed md:shadow-none col-span-8 md:bg-transparent md:relative justify-end w-full flex")
@@ -11,6 +11,8 @@
           span.block.absolute.w-5.bg-accent.transform.transition.duration-500.ease-in-out(aria-hidden='true' class='h-0.5' :class="{'opacity-0': open } ")
           span.block.absolute.w-5.bg-accent.transform.transition.duration-500.ease-in-out(aria-hidden='true' class='h-0.5' :class="{'-rotate-45': open, ' translate-y-1.5': !open}")
       ul(:class="open ? 'shadow-2xl bg-primary right-0 transition-all duration-500 ease-in-out pb-8' : 'right-hidden'" class="transition-all duration-500 ease-in-out flex flex-col md:grid grid-cols-5 my-10 md:pr-10 w-full md:w-auto sm:w-full absolute md:relative md:right-auto")
+        li(@click='open = false' class="md:hidden mt-12 py-1 mx-16 md:mx-0 md:block md:py-0 px-3 md:text-lg border-b border-transparent hover:border-accent hover:font-bold text-center transition duration-300 ease-in-out")
+          NuxtLink(to="/" class="block py-6 md:py-3" ) Strona Główna
         li(v-for="(route,index) in routes" @click='open = false' class="py-1 mx-16 md:mx-0 md:block md:py-0 px-3 md:text-lg border-b border-transparent hover:border-accent hover:font-bold text-center transition duration-300 ease-in-out")
           NuxtLink(:to="route.path" class="block py-6 md:py-3" ) {{route.name}}
 </template>
